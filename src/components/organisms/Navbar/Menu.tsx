f13141011ba8d75137c683f1e756bd655381ae70
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-09-15 10:40:28 +0700

    Refactor Navbar

--- src/components/organisms/Navbar/Menu.tsx
+++ src/components/organisms/Navbar/Menu.tsx
@@ -1,14 +1,24 @@
+import cx from "classnames";
+import Link from "next/link";
 interface MenuProps {
-  title?: string;
+  title: string;
+  active?: boolean;
+  href: string;
 }
-export default function Menu(props: MenuProps) {
-  const { title } = props;
+export default function Menu(props: Partial<MenuProps>) {
+  const { title, active, href = "/" } = props;
+  const classTitle = cx({
+    "nav-link": true,
+    active,
+  });
   return (
     <>
       <li className="nav-item my-auto">
-        <a className="nav-link active" aria-current="page" href="#">
-          {title}
-        </a>
+        <Link href={href}>
+          <a className={classTitle} aria-current="page">
+            {title}
+          </a>
+        </Link>
       </li>
     </>
   );
